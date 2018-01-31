f5cd47ab958d15446a1a7703fccf24e716105724
Maurice Murphy <murphym757@yahoo.com>
2018-01-30 21:32:49 -0500

    adding additional images for newer project and a new log for the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20161213030008) do
 
-  create_table "titles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
